42c1f79ac915e28bfa91c9ac4022c5ff6263981f
Tim <chent1101@gmail.com>
2015-11-09 13:27:53 -0700

    Initial commit

--- js/app.js
+++ js/app.js
@@ -12,6 +12,47 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+	/*--- Random number ---*/
+	var randNum = Math.floor(Math.random()*100) + 1;
+
+	var guessCount = 0;
+
+	/*--- Number guess ---*/
+	$("#guessButton").click(function() {
+		var guess = $("#userGuess").val();
+		var temp = '';
+		if (isNaN(guess)) {
+			$("#userGuess").val('');
+			return alert("Please enter number");
+		} else if (parseInt(guess) === randNum) {
+			$("#feedback").text("Winner Winner!!! Chicken Dinner!!!");
+			temp = "Winner!!!";
+			alert("You win!");
+			$("#userGuess").val('');
+		} else if (((parseInt(guess) > (randNum - 4)) && ((parseInt(guess) < (randNum))) || ((parseInt(guess) < (4 + randNum)) && ((parseInt(guess) > (randNum)))))) {
+			$("#userGuess").val('');
+			$("#feedback").text("Hot");
+			temp = "Hot";
+		} else if (((parseInt(guess) > (randNum - 10)) && ((parseInt(guess) < (randNum - 3))) || ((parseInt(guess) < (10 + randNum)) && ((parseInt(guess) > (randNum + 3)))))) {
+			$("#userGuess").val('');
+			$("#feedback").text("Warm");
+			temp = "Warm";
+		} else {
+			$("#userGuess").val('');
+			$("#feedback").text("Cold");
+			temp = "Cold";
+		};
+
+		guessCount++;
+		$("#count").text(guessCount);
+		$("#guessList").prepend("<li>" + "Guess #" + guessCount + ": " + guess + " " + temp + "</li>");
+	});
+
+	/*--- New game ---*/
+	$(".new").click(function() {
+		location.reload();
+	});
+
 });
 
 
